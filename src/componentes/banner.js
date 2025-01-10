aaf1901fe4c12f6d8db1b4937c2868cf722e77bb
DEV-BrunoSilvaa <brunosurf1005@gmail.com>
2025-01-10 03:15:13 -0300

    primeiros passos

---
+++ src/componentes/banner.js
@@ -0,0 +1,12 @@
+import './banner.css'
+
+function Banner() {
+    // JSX 
+    return(
+        <header className='banner'>
+        <img src="/imagens/banner.png" alt="O banner principar do organo" />
+        </header>
+    )
+}
+
+export default Banner 
