7171df5113214f9a7f7f91ac4227167789e624b2
DEV-BrunoSilvaa <brunosurf1005@gmail.com>
2025-01-18 16:50:09 -0300

    campo texto.css

--- src/Componentes/formulario/iindex.js
+++ src/Componentes/formulario/iindex.js
@@ -1,5 +1,5 @@
 import './formulario.css'
-import CampoTexto from '../CampoTexto/inndex'
+import CampoTexto from '../CampoTexto/index'
 import ListaSuspensa from '../ListaSuspensa/lista'
 import Botao from '../Botao/botao'
 import { useState } from 'react'
