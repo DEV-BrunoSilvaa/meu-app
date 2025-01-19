6eae6529fbc8c4c76857dbc52d1638b8e783bba1
DEV-BrunoSilvaa <brunosurf1005@gmail.com>
2025-01-19 02:42:26 -0300

    atualizando

---
+++ src/Componentes/CampoTexto/inndex.js
@@ -0,0 +1,22 @@
+
+import './campotextoo.css'
+
+const CampoTexto = (props) => {
+// onchange criase para dar guardar os valores escritos dentro dos campos de testo cria ele e depois monta o console.log 
+
+
+    
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
+    return (
+        <div className="campo-texto">
+            <label>
+                {props.label}
+                </label>
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
+        </div>
+    )
+}
+
+export default CampoTexto
